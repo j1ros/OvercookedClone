fe38917933fbcd1e7dfe9be811dba8c3f22f4f5b
j1ros <j1ros@mail.ru>
2024-07-22 17:32:57 +0300

    player move

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Overcooked
+{
+    public class GameInput : MonoBehaviour
+    {
+        private PlayerInputActions _playerInputActions;
+
+        private void Awake()
+        {
+            _playerInputActions = new PlayerInputActions();
+            _playerInputActions.Player.Enable();
+        }
+
+        public Vector2 GetMovementVectorNormilized()
+        {
+            Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+
+            inputVector = inputVector.normalized;
+            return inputVector;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Overcooked
+{
+    [RequireComponent(typeof(PlayerMovement))]
+    public class PlayerAnimation : MonoBehaviour
+    {
+        [SerializeField] private Animator _playerAnimator;
+        private PlayerMovement _playerMovement;
+
+        private void Awake()
+        {
+            _playerMovement = GetComponent<PlayerMovement>();
+        }
+
+        private void Update()
+        {
+            _playerAnimator.SetBool("IsWalking", _playerMovement.IsWalking);
+        }
+    }
+}
